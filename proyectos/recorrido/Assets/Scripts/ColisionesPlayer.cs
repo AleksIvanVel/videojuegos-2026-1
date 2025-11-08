605ccdbd96e8b59a1b20c8f01473a2e2aa0eb718
Madris1402 <ricmad142@gmail.com>
2025-11-07 23:16:30 -0600

    Sonidos, Texturas Arregladas, Video Adicional

--- proyectos/recorrido/Assets/Scripts/ColisionesPlayer.cs
+++ proyectos/recorrido/Assets/Scripts/ColisionesPlayer.cs
@@ -6,10 +6,15 @@ using UnityEngine.UI;
 public class ColisionesPlayer : MonoBehaviour
 {
     public AudioSource sonido;
-    public AudioClip agua;
+    public AudioClip bombagas;
+    public AudioClip policia;
+    public AudioClip jardin;
+    public AudioClip burger;
+    public AudioClip atm;
     public Image comida;
     public GameObject videoGasolinera;
     public GameObject videoSabrina;
+    public GameObject videoBurger;
     public GameObject videoWonderWoman;
     // Start is called before the first frame update
     void Start()
@@ -19,10 +24,31 @@ public class ColisionesPlayer : MonoBehaviour
         videoGasolinera.SetActive(false);
         videoSabrina.SetActive(false);
         videoWonderWoman.SetActive(false);
+        videoBurger.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider obj)
     {
+        if (obj.tag == "bombagas")
+        {
+            sonido.PlayOneShot(bombagas);
+        }
+        if (obj.tag == "burger")
+        {
+            sonido.PlayOneShot(burger);
+        }
+        if (obj.tag == "jardin")
+        {
+            sonido.PlayOneShot(jardin);
+        }
+        if (obj.tag == "atm")
+        {
+            sonido.PlayOneShot(atm);
+        }
+        if (obj.tag == "policia")
+        {
+            sonido.PlayOneShot(policia);
+        }
         if(obj.tag == "comida")
         {
             comida.gameObject.SetActive(true);
@@ -35,6 +61,10 @@ public class ColisionesPlayer : MonoBehaviour
         {
             videoSabrina.SetActive(true);
         }
+        if (obj.tag == "burgervid")
+        {
+            videoBurger.SetActive(true);
+        }
         if (obj.tag == "wonderwoman")
         {
             videoWonderWoman.SetActive(true);
@@ -43,6 +73,8 @@ public class ColisionesPlayer : MonoBehaviour
 
     private void OnTriggerExit(Collider obj)
     {
+        sonido.Stop();
+
         if (obj.tag == "comida")
         {
             comida.gameObject.SetActive(false);
@@ -58,6 +90,10 @@ public class ColisionesPlayer : MonoBehaviour
         if (obj.tag == "wonderwoman")
         {
             videoWonderWoman.SetActive(false);
+        }
+        if (obj.tag == "burgervid")
+        {
+            videoBurger.SetActive(false);
         }
     }
 }
